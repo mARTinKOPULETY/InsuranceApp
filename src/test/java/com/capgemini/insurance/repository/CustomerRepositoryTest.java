0e503c83948800f1b487b38ffd0c398f7ba6e9a0
mARTinKOPULETY <kopuletym@seznam.cz>
2023-04-19 14:02:06 +0200

    Added Repository testing class

---
+++ src/test/java/com/capgemini/insurance/repository/CustomerRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.capgemini.insurance.repository;
+
+import com.capgemini.insurance.entity.Customer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    @DisplayName("Shuld save the customer into the database")
+    void save(){
+        //Arrange
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+        customer.setFirstName("Jan");
+        customer.setLastName("Novak");
+        customer.setMiddleName("Josef");
+        customer.setEmail("johnjnovak@gmail.com");
+        customer.setPhoneNumber("666999666");
+        customer.setBirthDate(LocalDate.of(1990, 1, 1));
+        //Act
+        Customer updatedCustomer = customerRepository.save(customer);
+        //Assert
+        assertNotNull(updatedCustomer);
+        assertThat(updatedCustomer.getFirstName()).isEqualTo("Jan");
+    }
+
+    @Test
+    @DisplayName("Should find the customer by its id.")
+    void findById(){
+        //Arrange
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+        customer.setFirstName("Jan");
+        customer.setLastName("Novak");
+        customer.setMiddleName("Josef");
+        customer.setEmail("johnjnovak@gmail.com");
+        customer.setPhoneNumber("666999666");
+        customer.setBirthDate(LocalDate.of(1990, 1, 1));
+        //Act
+        Customer customerById = customerRepository.findById(customer.getCustomerId()).get();
+        //Assert
+        assertNotNull(customerById);
+        assertThat(customerById.getFirstName()).isEqualTo("Jan");
+    }
+}
